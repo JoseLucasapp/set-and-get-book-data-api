a666c3b2a7f442e633ca726d43d778b376b69df8
José Lucas <josluc2016@gmail.com>
2023-04-20 03:46:45 -0300

    fix: create error

--- src/app/usecases/CreateBooks.spec.ts
+++ src/app/usecases/CreateBooks.spec.ts
@@ -1,26 +1,19 @@
 import { InMemoryBooksRepository } from "../../../tests/repositories/in-memory-books-repository"
-import { Book } from "../../domain/entities/books"
 import { CreateBook } from "./CreateBook"
 
 describe('Create new book', () => {
     it('Should be able to create a new book', async () => {
         const booksRepository = new InMemoryBooksRepository()
 
-        const book = Book.create({
+        const newBook = new CreateBook(booksRepository)
+
+        const res = await newBook.execute({
             title: 'Animals farm',
             author: 'George Orwell',
             pages: 200
         })
 
-        booksRepository.items.push(book)
-
-        const newBook = new CreateBook(booksRepository)
-
-        const res = await newBook.execute({
-            title: book.props.title,
-            author: book.props.author,
-            pages: book.props.pages
-        })
+        booksRepository.items.push(res)
 
         expect(res).toBeTruthy()
     })
