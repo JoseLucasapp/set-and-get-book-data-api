3720438a0385a80a06bd1d5541ffc3ff79202d72
José Lucas <josluc2016@gmail.com>
2023-04-21 13:06:14 -0300

    feat: server started

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import 'dotenv/config'
+import express from 'express'
+import cors from 'cors'
+import { rateLimit } from 'express-rate-limit'
+
+const app = express()
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+    message: "Too many requests, please try again later"
+})
+const router = express.Router()
+
+app.use(express.json())
+app.use(cors())
+app.use(limiter)
+app.use(router)
+
+router.get('/', (req, res) => res.send(`<h1>Welcome to the home page !!!</h1>`))
+
+export default app
